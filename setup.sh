7efe799bb813feda74b064601ae1e2080058f5cf
kouki suzuki <ks.tetu.hachiouji@gmail.com>
2016-12-14 05:45:41 -0800

    changed to simple sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-DOT_FILES=(.vimrc .vim)
-
-for file in ${DOT_FILES[@]}
-do
-    ln -s $HOME/dotfiles/$file $HOME/$file
-done
-
-curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
-sh ./install.sh
-rm -rf install.sh
-
-
+mkdir -p $HOME/.vim/bundle/
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+bash ./neobundle_install.sh
